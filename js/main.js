d3509d1492491107ab2958e390503880660802eb
mma <123@qq.com>
2020-05-20 14:39:10 +0800

    添加样式

---
+++ js/main.js
@@ -0,0 +1,33 @@
+// 欢迎访问动画
+let startLi = document.querySelectorAll(".first-screen ul li");
+let body = document.querySelector("body");
+let firstScreen = document.querySelector(".first-screen");
+let next = document.querySelector(".first-screen .next-button");
+console.log(next);
+
+body.style.overflow = "hidden";
+next.addEventListener("click", function(event) {
+  console.log("14");
+  
+  // console.log(step);
+  let position = firstScreen.offsetHeight + 1;
+  
+  let timer = setInterval(function() {
+    // console.log(`position:${position}`);
+    let step = document.documentElement.scrollTop;
+    step += (position - step ) / 10;
+    step = Math.ceil(step);
+    if(step < position) {
+      document.documentElement.scrollTop = step;
+    }else {
+      body.style.overflow = "auto";
+      clearInterval(timer);
+    }
+    console.log(`step:${step}`);
+    
+  }, 50)
+  
+  
+})
+
+
